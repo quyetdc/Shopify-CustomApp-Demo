27a93de7826af5dcdf516610e3f373a176d0a8b8
Apple <apple@quyetdc.local>
2019-05-28 15:41:20 +0700

    Env variables

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,12 +1,15 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "518b229133b386457a38b0cd1fd2c8e4"
-  config.old_secret = "<old_secret>"
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.application_name = "Colin Code Shopify"
+  config.api_key = ENV['api_key']
+  config.secret = ENV['api_secret']
+  config.old_secret = ENV['api_secret']
+  config.scope = "read_orders, read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2019-04"
   config.session_repository = Shop
+
+  # config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+
 end
