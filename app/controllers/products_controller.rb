42c02867ab789a544cc3aed1c776748cd7956b95
quyetdc <quyetdc.uet@gmail.com>
2019-06-08 17:43:19 +0700

    Fetch Product Metafields

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class ProductsController < AuthenticatedController
+  def index
+    # @products = ShopifyAPI::Product.find(:all, params: { limit: 1, page: 1 })
+
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @webhooks = ShopifyAPI::Webhook.find(:all)
+  end
+
   def show
-    Struct.new("Image", :src, :alt)
+    Struct.new("Metafield", :src, :alt)
 
     @product = ShopifyAPI::Product.find(params[:id])
     @images = []
@@ -10,21 +17,28 @@ class ProductsController < AuthenticatedController
     # store header for request call to Shopify API
     headers = {"X-Shopify-Access-Token" => @shop_session.token}
 
-    @product.images.each do |image|
-      id = image.id.to_s
-      src = image.src
+    @metafields = HTTParty.get('https://quyet-akronym-store.myshopify.com/admin/metafields.json?metafield[owner_id]=' +
+                                params[:id] +'&metafield[owner_resource]=product',
+                                :headers => headers).values[0].map { |metafield| OpenStruct.new metafield }
 
-      #make the request and store it in a variable
-      metafields = HTTParty.get('https://quyet-akronym-store.myshopify.com/admin/metafields.json?metafield[owner_id]=' +
-                                    id +'&metafield[owner_resource]=product_image',
-                                :headers => headers).values
 
-      metafields.first.present? ? alt = metafields.first['value'] : alt = @product.title
 
-      image = Struct::Image.new(src, alt)
+    # @product.images.each do |image|
+    #   id = image.id.to_s
+    #   src = image.src
+    #
+    #   #make the request and store it in a variable
+    #   metafields = HTTParty.get('https://quyet-akronym-store.myshopify.com/admin/metafields.json?metafield[owner_id]=' +
+    #                                 id +'&metafield[owner_resource]=product_image',
+    #                             :headers => headers).values
+    #
+    #   metafields.first.present? ? alt = metafields.first['value'] : alt = @product.title
+    #
+    #   image = Struct::Image.new(src, alt)
+    #
+    #   #add the returned data to the @image_metafiels array
+    #   @images.push(image)
+    # end
 
-      #add the returned data to the @image_metafiels array
-      @images.push(image)
-    end
   end
 end
